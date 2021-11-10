9056955450a9cc19808a55a95413fb6ab6674f81
Ibrahim Mubarek <42622248+imubarek@users.noreply.github.com>
2021-11-09 22:48:48 -0500

    Finished more functions

--- src/main.js
+++ src/main.js
@@ -2,4 +2,11 @@ import {sumToString} from "./mild/mild_1.js";
 import {getIncreasingArray} from "./mild/mild_1.js";
 import {maxAndMin} from "./mild/mild_1.js";
 import {countArray} from "./mild/mild_1.js";
-countArray( [1,2,3,4,  [1,2]   ]); 
+import {identifyVariable} from "./mild/mild_2.js";
+import {identifyArray} from "./mild/mild_2.js";
+import {removeKey} from "./mild/mild_2.js";
+import {removeKeyNonDestructive} from "./mild/mild_2.js";
+import {removeKeys} from "./mild/mild_2.js";
+var dict = {"1" : "hello" , "2" : "there"}
+console.log(removeKeys(dict,["2","1"])) ; 
+console.log(dict) ; 
--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -6,7 +6,7 @@
  * returns: { type: 'number', value: 4 }
  */
 export function identifyVariable(variable) {
-
+   return {"type" : typeof("1"), "value" : variable} ; 
 }
 
 
@@ -24,7 +24,11 @@ export function identifyVariable(variable) {
 
  */
 export function identifyArray(array) {
-
+   var typeArray = [] ; 
+   for(let x = 0 ; x<array.length ; x++){
+      typeArray.push(identifyVariable(array[x])) ; 
+   }
+   return typeArray ; 
 }
 
 /**
@@ -44,7 +48,7 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
-
+   delete object[key] ; 
 }
 
 /**
@@ -64,7 +68,9 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-
+   var tempObject = Object.assign({} , object) ;  
+   delete tempObject[key] ; 
+   return tempObject ; 
 }
 
 /**
@@ -89,5 +95,9 @@ export function removeKeyNonDestructive(object, key) {
  * @return {*} The object with its keys removed.
  */
 export function removeKeys(object, keyList) {
-
+   var tempObject = Object.assign({} , object) ;  
+   for(let x = 0 ; x<keyList.length ; x++){
+      delete tempObject[keyList[x]] ; 
+   }
+   return tempObject ;
 }
